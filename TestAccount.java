4452cb8b579e1e5c9e01f4a273bd186466809b9f
Vipul Saklani <vipul.cse17@nituk.ac.in>
2021-08-27 21:34:32 +0530

    Create TestAccount.java

---
+++ TestAccount.java
@@ -0,0 +1,42 @@
+package com.lti.ex11;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.lti.ex11.*;
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations="classpath:appctx.xml")
+
+public class TestAccount {
+	@Autowired
+	private ApplicationContext appctx;
+	@Autowired
+	private Account tc;
+	@Test
+	public void testValidDeposit() throws AmountException
+	{
+	
+		tc.deposit(1000.0);
+	}
+	@Test
+	public void testInvalidDeposit() throws AmountException
+	{
+		tc.deposit(-102);
+	}
+	@Test
+	public void testValidWithdraw() throws AmountException, BalanceException
+	{
+		tc.balance=10000;
+		tc.withdraw(100);
+	}
+	@Test
+	public void testInValidWithdraw() throws AmountException, BalanceException
+	{
+		tc.withdraw(100000);
+	}
+
+}
